b1adb72dedadcfd132cbb62d0b34a4edd1986fa6
StarWong <ai92425@gmail.com>
2020-12-13 10:18:16 +0800

    add callback functions

--- ALPC_Server_Example.pas
+++ ALPC_Server_Example.pas
@@ -37,5 +37,21 @@ procedure TMainForm.Button2Click(Sender: TObject);
 begin
 StopServer;
 end;
-
+procedure sum(const user:PViewCache;const host:PReadIn);
+function sum(const a,b:Integer):Integer;
+begin
+    Result:=a + b;
+end;
+var
+list:ret_Offsets;
+begin
+list[0]:=1;
+list[1]:=4;
+host.Push(user);
+user.Marshal(list);
+PInteger(user.return(0))^:=sum(PInteger(host^*0)^ ,PInteger(host^*1)^);
+user.Sign(True);
+end;
+initialization
+AddFunc('sum',sum);
 end.
